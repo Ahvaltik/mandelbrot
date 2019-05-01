5804bc0b5473c9572bde799f6bd4011a049f79c3
Ahvaltik <pgziewiec@gmail.com>
2019-05-01 15:31:16 +0200

    added colors and parameters

--- sketch.js
+++ sketch.js
@@ -1,6 +1,9 @@
 var s;
+var boundary = 1024;
+var resolution = 512;
+
 function setup() {
-    createCanvas(600, 600);
+    createCanvas(resolution, resolution);
     s = new Mandelbrot();
     background(0);
     s.show();
@@ -15,15 +18,17 @@ function getSqr(a, b) {
 
 function getColor(i)
 {
-	//let a = Color('darkblue');
-	//let b = Color('red');
-	return log(i) * 64;//lerpColor(a, b, log(i) / 4);
+	if (i == boundary)
+		return color('black');
+	let a = color('darkblue');
+	let b = color('white');
+	return lerpColor(a, b, log(i) / 4);
 }
 
 function getMandelbrot(a, b) {
 	let a1 = a, b1 = b;
 	let i;
-	for (i = 0; i < 2048 & getSqr(a1, b1) < 4; i++)
+	for (i = 0; i < boundary & getSqr(a1, b1) < 4; i++)
 	{
 		let nexta = a1 * a1 - b1 * b1 + a;
 		b1 = 2 * a1 * b1 + b;
@@ -34,9 +39,9 @@ function getMandelbrot(a, b) {
 
 function Mandelbrot() {
     this.show = function () {
-        for (y = 0; y < 600; y++) {
-			for (x = 0; x < 600; x++) {
-				stroke(getMandelbrot(x / 150 - 2 , y / 150 - 2));
+        for (y = 0; y < resolution; y++) {
+			for (x = 0; x < resolution; x++) {
+				stroke(getMandelbrot( 4 * x / resolution - 2 , 4 * y / resolution - 2));
 				point(x, y);
 			}
         }
